f4a7d30b4211627aa6e6110ce012def368ce4e0b
PABLO MORERA <PABLO MORERA@DESKTOP-OPTDJEA>
2023-09-12 12:25:45 -0600

    Clase Data

---
+++ src/main/java/Data/Data.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Data;
+
+import Logic.Instrumento;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author PABLO MORERA
+ */
+public class Data {
+    private List<Instrumento> instrumentos;
+
+    public Data() {
+        instrumentos = new ArrayList<>();
+        instrumentos.add(new Instrumento("TER","Termómetro","Grados Celcius") );
+        instrumentos.add(new Instrumento("BAR","Barómetro","PSI") );
+    }
+
+    public List<Instrumento> getInstrumentos() {
+        return instrumentos;
+    }
+    
+    
+}
