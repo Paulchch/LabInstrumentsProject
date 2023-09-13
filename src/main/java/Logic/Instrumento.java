117a89d2c798f2d596969daeec101e86bc9766b4
PABLO MORERA <PABLO MORERA@DESKTOP-OPTDJEA>
2023-09-13 17:17:55 -0600

    Nueva estructura TipoInstrumento hereda a Instrumento

---
+++ src/main/java/Logic/Instrumento.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Logic;
+
+/**
+ *
+ * @author PABLO MORERA
+ */
+public class Instrumento extends TipoInstrumento{
+    private String serie;
+    private String tipo;
+    private String descripcion;
+    private String minimo;
+    private String maximo;
+    private String tolerancia;
+
+    public Instrumento(String serie, String tipo, String descripcion, String minimo, String maximo, String tolerancia) {
+        this.serie = serie;
+        this.tipo = tipo;
+        this.descripcion = descripcion;
+        this.minimo = minimo;
+        this.maximo = maximo;
+        this.tolerancia = tolerancia;
+    }
+
+    public String getSerie() {
+        return serie;
+    }
+
+    public void setSerie(String serie) {
+        this.serie = serie;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getMinimo() {
+        return minimo;
+    }
+
+    public void setMinimo(String minimo) {
+        this.minimo = minimo;
+    }
+
+    public String getMaximo() {
+        return maximo;
+    }
+
+    public void setMaximo(String maximo) {
+        this.maximo = maximo;
+    }
+
+    public String getTolerancia() {
+        return tolerancia;
+    }
+
+    public void setTolerancia(String tolerancia) {
+        this.tolerancia = tolerancia;
+    }
+    
+}
