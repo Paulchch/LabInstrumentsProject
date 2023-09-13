f32efbbb41f1b9f8d9ad8c3e1932dea20502ee5f
David-1512 <davidjserranom76@gmail.com>
2023-09-13 16:00:59 -0600

    AddIntrumento XML

--- src/main/java/XML_DOM/XMLIntrumentos.java
+++ src/main/java/XML_DOM/XMLIntrumentos.java
@@ -9,10 +9,12 @@ import org.w3c.dom.*;
 import javax.xml.parsers.*;
 import java.io.*;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import org.xml.sax.SAXException;
 /**
  *
  * @author David
@@ -43,25 +45,58 @@ public class XMLIntrumentos {
                 Element instrumentos = document.createElement("Instrumentos");
                 document.appendChild(instrumentos);
                 
-                for(int i = 1; i <= datos.getInstrumentos().size();i++){
-                Element instrumento = document.createElement("Instrumento"+i);
-                instrumentos.appendChild(instrumento);
-               
-                inst = datos.getInstrumentos().get(i);
+                Element inst1 = document.createElement("Instrumento1");
+                instrumentos.appendChild(inst1);
                 
-                 Element codigo = document.createElement("Codigo");
-                codigo.appendChild(document.createTextNode(inst.getCodigo()));
-                 instrumento.appendChild(codigo);
+                Element cod1 = document.createElement("Codigo");
+                cod1.appendChild(document.createTextNode("TER"));
+                inst1.appendChild(cod1);
                  
-                 Element nombre = document.createElement("Nombre");
-                nombre.appendChild(document.createTextNode(inst.getNombre()));
-                instrumento.appendChild(nombre);
+                Element nom1 = document.createElement("Nombre");
+                nom1.appendChild(document.createTextNode("Termómetro"));
+                inst1.appendChild(nom1);
+                
+                Element uni1 = document.createElement("Unidad");
+                uni1.appendChild(document.createTextNode("Grados Celsius"));
+                inst1.appendChild(uni1);
+
+                Element inst2 = document.createElement("Instrumento2");
+                instrumentos.appendChild(inst2);
+                
+                Element cod2 = document.createElement("Codigo");
+                cod2.appendChild(document.createTextNode("BAR"));
+                inst2.appendChild(cod2);
+                 
+                Element nom2 = document.createElement("Nombre");
+                nom2.appendChild(document.createTextNode("Barómetro"));
+                inst2.appendChild(nom2);
                 
                    
-                Element unidad = document.createElement("Unidad");
-                unidad.appendChild(document.createTextNode(inst.getUnidad()));
-                 instrumento.appendChild(unidad);
-                }
+                Element uni2 = document.createElement("Unidad");
+                uni2.appendChild(document.createTextNode("PSI"));
+                inst2.appendChild(uni2);
+                 
+                 
+//                
+//                for(int i = 1; i <= datos.getInstrumentos().size();i++){
+//                Element instrumento = document.createElement("Instrumento"+i);
+//                instrumentos.appendChild(instrumento);
+//               
+//                inst = datos.getInstrumentos().get(i);
+//                
+//                 Element codigo = document.createElement("Codigo");
+//                codigo.appendChild(document.createTextNode(inst.getCodigo()));
+//                 instrumento.appendChild(codigo);
+//                 
+//                 Element nombre = document.createElement("Nombre");
+//                nombre.appendChild(document.createTextNode(inst.getNombre()));
+//                instrumento.appendChild(nombre);
+//                
+//                   
+//                Element unidad = document.createElement("Unidad");
+//                unidad.appendChild(document.createTextNode(inst.getUnidad()));
+//                 instrumento.appendChild(unidad);
+//                }
 
                 // create the xml file
                 //transform the DOM Object to an XML File
@@ -85,7 +120,48 @@ public class XMLIntrumentos {
         }
     }
     
+     public boolean AddInstrumento(Instrumento ints) throws TransformerConfigurationException, TransformerException
+    {
+        boolean result = false;
+       
+        try {
+            File xmlFile = new File(xmlFilePath);
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            if(!xmlFile.exists())
+                return result;
+            Document document = builder.parse(xmlFile);
+     
+            Node root = document.getElementsByTagName("Instrumentos").item(0);
+
+            Element newInst = document.createElement("Intrumento"+datos.getInstrumentos().size());
     
+            root.appendChild(newInst);
+                
+            inst = datos.getInstrumentos().get(datos.getInstrumentos().size()-1);
+                   
+            Element cod = document.createElement("Codigo");
+            cod.appendChild(document.createTextNode(inst.getCodigo()));
+            newInst.appendChild(cod);
+                 
+            Element nom = document.createElement("Nombre");
+            nom.appendChild(document.createTextNode(inst.getNombre()));
+            newInst.appendChild(nom);
+                     
+            Element uni = document.createElement("Unidad");
+            uni.appendChild(document.createTextNode(inst.getUnidad()));
+            newInst.appendChild(uni);
     
-    
+                 
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource domSource = new DOMSource(document);
+            StreamResult streamResult = new StreamResult(new File(xmlFilePath));
+
+            transformer.transform(domSource, streamResult);
+            
+        } catch (ParserConfigurationException | IOException | SAXException pce) {
+        }    
+         return result; 
+    } 
 }
